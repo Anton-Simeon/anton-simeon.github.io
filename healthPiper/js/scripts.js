0f0cb77fbe33f980c91ca283740ff12202e066db
volodymyr <antonsimeonvladimir@gmail.com>
2021-05-01 00:29:20 +0300

    update

--- healthPiper/js/scripts.js
+++ healthPiper/js/scripts.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
 	$('.footer-top').click(function(){
 		$("html, body").animate({ scrollTop: 0 }, 100);
 	});
+	$(window).scroll(function(){
+		console.log('asdsa');
+		if($(window).scrollTop() > $(window).height() ) {
+			$('.footer-top').fadeIn();
+		}else {
+			$('.footer-top').fadeOut();
+		}
+	});
 });
