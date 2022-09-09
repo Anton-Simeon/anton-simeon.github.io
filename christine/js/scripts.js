1b20306f7372a82dbf90bba67207f7bbbb429d04
Volodymyr <volodymyr.anton-simeon@baex.ca>
2022-09-09 11:18:38 +0300

    update

---
+++ christine/js/scripts.js
@@ -0,0 +1,15 @@
+// $(function() {
+// 	function scrollImg() {
+// 		var scrollPosition = $( window ).scrollTop();
+// 		var windowHeight = $( window ).height();
+// 		var maxOffset = 250;
+
+// 		var marginTop =  (maxOffset / windowHeight) * scrollPosition - 40;
+// 		$('.hero-img > div').css('margin-top', marginTop);
+// 	}
+// 	scrollImg();
+// 	$( window ).scroll(function() {
+// 		scrollImg();
+// 	});
+// 	scrollImg();
+// });
